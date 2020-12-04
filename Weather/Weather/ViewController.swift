464f57005b7400f37dbf31870f95c636f78b178c
Andrey Kovalevskiy <andrei.kavaleuski@gmail.com>
2020-12-04 18:10:31 +0300

    Add initial Weather project

---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Andrey Kovalevskiy on 4.12.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
