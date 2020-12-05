a1aec84e593c507aeea28af5dc6a326d88dc1715
Andrey Kovalevskiy <andrei.kavaleuski@gmail.com>
2020-12-05 23:38:47 +0300

    Implement textField for city search

--- Weather/Weather/Controller/WeatherViewController.swift
+++ Weather/Weather/Controller/WeatherViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class WeatherViewController: UIViewController {
-
+    
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
@@ -16,11 +16,34 @@ class WeatherViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchTextField.delegate = self
+        
     }
     
     @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
     }
     
 }
 
+extension WeatherViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text == "" {
+            textField.placeholder = "type something"
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextField.text = ""
+    }
+}
