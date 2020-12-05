d8400ed52c2036eaadd5050b0b7f7d47c74c1a3c
Andrey Kovalevskiy <andrei.kavaleuski@gmail.com>
2020-12-05 15:44:36 +0300

    Сonnect UI to code and rename ViewController to WeatherViewController

---
+++ Weather/Weather/WeatherViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Andrey Kovalevskiy on 4.12.20.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+    }
+    
+}
+
